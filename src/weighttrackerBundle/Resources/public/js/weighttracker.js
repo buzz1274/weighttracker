d5c880032d43dced36b1073046ba84c2168c3b08
David Exelby <david@sulaco.co.uk>
2014-04-30 03:50:46 +0100

    .

---
+++ src/weighttrackerBundle/Resources/public/js/weighttracker.js
@@ -0,0 +1,13 @@
+define(function(require, exports, module) {
+    "use strict";
+
+    var jq = require('jquery');
+
+    console.log("HERE");
+
+    var WeightTracker = {
+    };
+
+    module.exports = WeightTracker;
+
+});

06caa96a6b1d4343ef81ae23859d70014f14748f
David Exelby <david@sulaco.co.uk>
2014-05-01 03:25:36 +0100

    javascript libraries

---
+++ src/weighttrackerBundle/Resources/public/js/app.js
@@ -0,0 +1,13 @@
+alert("HERE");
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});

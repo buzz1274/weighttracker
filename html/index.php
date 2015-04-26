167bae3d58f29bd20f05f049d4c8f5f5e7e7ee0c
David Exelby <david@sulaco.co.uk>
2015-04-26 16:10:24 +0100

    hello world phalcon

--- html/index.php
+++ html/index.php
@@ -1,3 +1,42 @@
 <?php
 
-	phpinfo();
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\Application;
+use Phalcon\DI\FactoryDefault;
+
+try {
+
+	// Register an autoloader
+	$loader = new Loader();
+	$loader->registerDirs(array(
+		'../app/controllers/',
+		'../app/models/'
+	))->register();
+
+	// Create a DI
+	$di = new FactoryDefault();
+
+	// Setup the view component
+	$di->set('view', function(){
+		$view = new View();
+		$view->setViewsDir('../app/views/');
+		return $view;
+	});
+
+	// Setup a base URI so that all generated URIs include the "tutorial" folder
+	$di->set('url', function(){
+		$url = new Url();
+		$url->setBaseUri('/');
+		return $url;
+	});
+
+	// Handle the request
+	$application = new Application($di);
+
+	echo $application->handle()->getContent();
+
+} catch(\Exception $e) {
+	echo "PhalconException: ", $e->getMessage();
+}

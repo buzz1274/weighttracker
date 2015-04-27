c2c923e7939d0d571d93b16856e5cb21bfd0e978
David Exelby <david@sulaco.co.uk>
2015-04-27 20:09:02 +0100

    hello world

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,7 @@ use Phalcon\Mvc\Controller;
 class IndexController extends Controller {
 
     public function indexAction() {
-        echo "<h1>Hello!</h1>";
+        $this->view->message = "Hello World";
     }
 
 }
--- html/index.php
+++ html/index.php
@@ -2,41 +2,54 @@
 
 use Phalcon\Loader;
 use Phalcon\Mvc\View;
+use Phalcon\Mvc\View\Engine\Volt;
 use Phalcon\Mvc\Url as UrlProvider;
 use Phalcon\Mvc\Application;
 use Phalcon\DI\FactoryDefault;
 
 try {
 
-	// Register an autoloader
-	$loader = new Loader();
-	$loader->registerDirs(array(
-		'../app/controllers/',
-		'../app/models/'
-	))->register();
+    // Register an autoloader
+    $loader = new Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
 
-	// Create a DI
-	$di = new FactoryDefault();
+    // Create a DI
+    $di = new FactoryDefault();
 
-	// Setup the view component
-	$di->set('view', function(){
-		$view = new View();
-		$view->setViewsDir('../app/views/');
-		return $view;
-	});
+    // Setup the view component
+    $di->set('view', function(){
+        $view = new View();
+        $view->setViewsDir('../app/views/');
 
-	// Setup a base URI so that all generated URIs include the "tutorial" folder
-	$di->set('url', function(){
-		$url = new Url();
-		$url->setBaseUri('/');
-		return $url;
-	});
+        $view->registerEngines(array(
+            ".volt" => function($view, $di) {
+                $volt = new Volt($view, $di);
 
-	// Handle the request
-	$application = new Application($di);
+                //set some options here
 
-	echo $application->handle()->getContent();
+                return $volt;
+            }
+        ));
+
+
+        return $view;
+    });
+
+    // Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new Url();
+        $url->setBaseUri('/');
+        return $url;
+    });
+
+    // Handle the request
+    $application = new Application($di);
+
+    echo $application->handle()->getContent();
 
 } catch(\Exception $e) {
-	echo "PhalconException: ", $e->getMessage();
+    echo "PhalconException: ", $e->getMessage();
 }

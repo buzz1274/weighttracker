5547252918ec00ca605e485a5e2d916bde2d4feb
David Exelby <david@sulaco.co.uk>
2015-05-05 11:27:19 +0100

    templates & routes

---
+++ app/routes/weight.js
@@ -0,0 +1,6 @@
+export default Ember.Route.extend({
+  setupController: function(controller) {
+    // Set the IndexController's `title`
+    controller.set('title', 'Display graph & table');
+  }
+});

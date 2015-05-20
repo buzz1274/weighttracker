dbff66c7dda14ea65f5d7e8de0ad232f879a0422
Dave <david@sulaco.co.uk>
2015-05-21 00:52:15 +0100

    updates

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,16 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var unwatchedTree = require('broccoli-unwatched-tree');
 
-var app = new EmberApp({});
+//var apiTree = new UnwatchedTree('public/api');
+var app = new EmberApp({
+  trees: {
+    'public': unwatchedTree('public/api')
+  }
+});
+
+EmberApp()
 
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('bower_components/moment/moment.js');

e5f7c8f03504dfb3e9dbf2ab657bdd78bd0a4442
buzz1274 <david@sulaco.co.uk>
2015-10-11 11:15:07 +0100

    remove auto logout message if error generated when logging in

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -17,6 +17,7 @@ export default Ember.Controller.extend({
           that.transitionToRoute('error');
         } else {
           that.set('errorMessage', message);
+          that.set('autoLogout', false);
           that.set('username', '');
           that.set('password', '');
         }

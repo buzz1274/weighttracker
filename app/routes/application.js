011c50440f651982691a7b3c81423ba1b5361bca
buzz1274 <david@sulaco.co.uk>
2015-10-28 18:59:15 +0000

    force reload of /login so user data is removed when logging out

--- app/routes/application.js
+++ app/routes/application.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
     sessionInvalidationSucceeded() {
       "use strict";
 
-      this.transitionTo('login');
+      window.location = '/login';
     }
   },
   setupAutoLogout: function() {

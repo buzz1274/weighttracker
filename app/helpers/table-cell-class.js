fa469fa6a8a3a938381582341bcd21a55b98931c
buzz1274 <david@sulaco.co.uk>
2016-06-11 11:33:46 +0100

    .

--- app/helpers/table-cell-class.js
+++ app/helpers/table-cell-class.js
@@ -8,13 +8,13 @@ export default Ember.Handlebars.makeBoundHelper(
     return 'success';
   } else {
     if(currentWeight < targetWeight) {
-      if(change <= 0) {
+      if(change < 0) {
         return 'danger';
       } else {
         return 'success';
       }
     } else if(currentWeight > targetWeight) {
-      if(change >= 0) {
+      if(change > 0) {
         return 'danger';
       } else {
         return 'success';

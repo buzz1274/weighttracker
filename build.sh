5efdd8d3d44b156a44fcf0d4a22843b41dc276b6
buzz1274 <david@sulaco.co.uk>
2022-06-05 17:07:20 +0100

    prebuilt js

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-cd "$DIR/api"
-composer install
-cd "$DIR"
-npm install
-bower install --allow-root
-ember build --environment production
+echo 1
 

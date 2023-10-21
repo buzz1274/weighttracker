e959a2c71224e61685aa7c5ff19e1e719e2c9a5c
David <david@sulaco.co.uk>
2023-10-21 13:36:29 +0100

    pre-commit

--- weights/models.py
+++ weights/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.db.models import DateField, DecimalField
 
+
+
+
 class Weight(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,

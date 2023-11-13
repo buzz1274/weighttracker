1ad8bef41ef752fad87dcd07bd38b04e66e8ece2
David Exelby <david@sulaco.co.uk>
2023-11-13 12:59:24 +0000

    redirect nginx on 404 to /

--- bin/housekeeping.sh
+++ bin/housekeeping.sh
@@ -2,7 +2,7 @@ BACKUPS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 BACKUP_FILENAME=weighttracker_"$(date '+%Y-%m-%d')".sql
 S3_BUCKET=s3://blta/backups/weighttracker/
 
-docker exec -it weighttracker-backend bash -c 'PGPASSWORD=$DB_PASSWORD pg_dump -U$DB_USERNAME -h$DB_HOST' > "$BACKUPS_DIR$BACKUP_FILENAME"
+docker exec -it weighttracker-backend bash -c 'PGPASSWORD=$DB_PASSWORD pg_dump --inserts -U$DB_USERNAME -h$DB_HOST' > "$BACKUPS_DIR$BACKUP_FILENAME"
 
 if [ -f "$BACKUPS_DIR$BACKUP_FILENAME" ]; then
     aws s3 cp  "$BACKUPS_DIR$BACKUP_FILENAME" "$S3_BUCKET""$BACKUP_FILENAME"

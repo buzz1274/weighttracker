6a2bb037bf5da4a4fa3d97269f86f5af3fc053c9
David <david@sulaco.co.uk>
2023-11-28 01:31:47 +0000

    split settings for production & development

--- weighttracker/settings/development.py
+++ weighttracker/settings/development.py
@@ -1,11 +1,6 @@
 from .production import *  # noqa: F403, F401
 
 DEBUG = True
-ALLOWED_HOSTS = [
-    "weighttracker-backend",
-    "127.0.0.1",
-    "dev.weighttracker.zz50.co.uk",
-]
 CSRF_TRUSTED_ORIGINS = [
     "http://dev.weighttracker.zz50.co.uk",
 ]
--- weighttracker/settings/production.py
+++ weighttracker/settings/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = [
-    "127.0.0.1",
+    "weighttracker-backend",
 ]
 
 CSRF_TRUSTED_ORIGINS = [

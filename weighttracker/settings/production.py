232b2f967da0197d00b53740509a0e46d9df6beb
David <david@sulaco.co.uk>
2023-11-28 01:25:39 +0000

    split settings for production & development

--- weighttracker/settings/production.py
+++ weighttracker/settings/production.py
@@ -27,6 +27,9 @@ SECRET_KEY = (
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://weighttracker.zz50.co.uk",

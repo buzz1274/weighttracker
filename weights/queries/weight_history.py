af060c426f26a78d090a74e7e6469a49018103e7
David <david@sulaco.co.uk>
2023-11-29 06:29:34 +0000

    fix weight history query

--- weights/queries/weight_history.py
+++ weights/queries/weight_history.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 from weights.models import Weight
 
 
@@ -19,11 +17,13 @@ class WeightHistory:
 
         return Weight.objects.raw(
             """
-            SELECT w.id, weights.date,
+            SELECT DISTINCT ON (w.id) id, weights.date,
                    weights.date_difference, w.weight,
                    last_week.last_week_date AS last_week_date,
                    last_week.weight AS last_week_weight,
-                   TO_CHAR(float8 (w.weight - last_week.weight), 'FM0.00') AS change
+                   TO_CHAR(
+                    float8 (w.weight - last_week.weight), 'FM0.00'
+                   ) AS change
             FROM (SELECT   w.date,
                            MIN(ABS((lw.date + 7) - w.date)) AS date_difference
                   FROM     weights_weight w
@@ -41,8 +41,9 @@ class WeightHistory:
                    WHERE w.user_id = 1
                    GROUP BY w.date, lw.weight, lw.date
                    ORDER BY w.date DESC, date_difference ASC
-                  ) AS last_week ON (    last_week.date_difference = weights.date_difference
-                                     AND w.date = last_week.date)
-            ORDER BY weights.date DESC
+                  ) AS last_week ON (
+                            last_week.date_difference = weights.date_difference
+                        AND w.date = last_week.date)
+            ORDER BY w.id DESC, weights.date DESC
             """
         )

88446b461ad872e90436bf2c7526a7747461dcca
David <david@sulaco.co.uk>
2024-12-26 21:02:58 +0000

    infra changes

--- backend/bin/start_container.sh
+++ backend/bin/start_container.sh
@@ -1,7 +1,9 @@
 sleep 30
 
 if [ "$ENVIRONMENT" == "DEVELOPMENT" ] ; then
-  gunicorn weighttracker.wsgi:application --bind=0.0.0.0:8000 --reload --log-level debug
+  sleep infinity
+  #gunicorn weighttracker.wsgi:application --bind=0.0.0.0:8000 --reload --log-level debug
 else
-  gunicorn weighttracker.wsgi:application --bind=0.0.0.0:8000 --log-level warning
+  sleep infinity
+  #gunicorn weighttracker.wsgi:application --bind=0.0.0.0:8000 --log-level warning
 fi

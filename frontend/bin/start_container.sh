f3ffe7c276c2beb559d23de7e58319e94fe7da52
David <david@sulaco.co.uk>
2024-12-31 19:51:43 +0000

    start script

--- frontend/bin/start_container.sh
+++ frontend/bin/start_container.sh
@@ -1,6 +1,6 @@
 if [ "$ENVIRONMENT" == "DEVELOPMENT" ] ; then
   npm install && npm run dev
 else
-  cp -r ../weighttracker.zz50.co.uk/dist .
+  cd /opt/weighttracker_frontend_public/ && cp -r ../weighttracker.zz50.co.uk/dist .
   sleep infinity
 fi

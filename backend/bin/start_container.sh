ba1d4b97a269d828b6f6fd8a3ba7720d23aa1939
David <david@sulaco.co.uk>
2024-12-31 10:48:54 +0000

    updated start command

--- backend/bin/start_container.sh
+++ backend/bin/start_container.sh
@@ -1,5 +1,8 @@
 sleep 30
 
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
 if [ "$ENVIRONMENT" == "DEVELOPMENT" ] ; then
   gunicorn weighttracker.wsgi:application --bind=0.0.0.0:8000 --reload --log-level debug
 else

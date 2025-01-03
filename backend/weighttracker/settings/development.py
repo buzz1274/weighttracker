324dbc28eba36d050a9a54dc5a95b71310871646
David <david@sulaco.co.uk>
2025-01-03 15:20:48 +0000

    debugger

--- backend/weighttracker/settings/development.py
+++ backend/weighttracker/settings/development.py
@@ -1,5 +1,18 @@
 from .production import *  # noqa: F403, F401
 
+try:
+    import pydevd_pycharm
+
+    pydevd_pycharm.settrace(
+        "host.docker.internal",
+        port=10002,
+        stdoutToServer=True,
+        stderrToServer=True,
+    )
+except Exception:
+    print("unable to run debugger")
+    pass
+
 DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
     "https://dev.weighttracker.zz50.co.uk",
@@ -9,6 +22,6 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ORIGIN_WHITELIST = [
     "http://dev.weighttracker.zz50.co.uk:5123",
 ]
-ALLOWED_HOSTS += [
+ALLOWED_HOSTS += [  # noqa: F405
     "dev.weighttracker.zz50.co.uk",
 ]

0b2a00458d3733dcd4aeb071e3dba36aaaa4c651
David <david@sulaco.co.uk>
2025-01-17 17:42:36 +0000

    .

--- backend/weighttracker/settings/production.py
+++ backend/weighttracker/settings/production.py
@@ -16,8 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
--- frontend/src/models/weightModel.ts
+++ frontend/src/models/weightModel.ts
@@ -1,5 +1,41 @@
-class weightModel {
-  constructor() {
-    console.log('HERE')
+import { ref } from 'vue'
+
+export class weightModel {
+  loaded = ref()
+  weights = ref([])
+  weight_values = ref([])
+  weight_dates = ref([])
+
+  get(): void {
+    console.log('FETCH')
+    fetch('https://' + window.location.hostname + '/api/user/weights/', { method: 'GET' })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        this.weights.value = data
+        //this.loaded.value = true
+      })
+      .then(() => {
+        this.extract_values_and_dates()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  extract_values_and_dates() {
+    console.log('EXTRACT')
+
+    this.weight_values.value = this.weights.reduce((weights, weight) => {
+      weights.push(weight['weight_kg'])
+      return weights
+    }, [])
+
+    this.weight_dates.value = this.weights.reduce((dates, weight) => {
+      dates.push(weight['date'])
+      return dates
+    }, [])
+
+    console.log(this.weight_dates)
   }
 }

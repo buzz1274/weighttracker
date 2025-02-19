af2446daad9d4525d2f0129c8d5f45ea1193665d
David <david@sulaco.co.uk>
2025-02-19 14:10:46 +0000

    use env vars for creating database

--- bin/post_install.sh
+++ bin/post_install.sh
@@ -4,13 +4,13 @@ sleep 10
 
 ENVIRONMENT=$1
 
-docker exec postgres bash -c "psql -v ON_ERROR_STOP=1 --username 'postgres' <<-EOSQL
-    CREATE DATABASE weight_tracker;
-    CREATE USER weight_tracker;
-    ALTER DATABASE weight_tracker OWNER TO weight_tracker;
-    GRANT ALL PRIVILEGES ON DATABASE weight_tracker to weight_tracker;
-    ALTER USER weight_tracker WITH PASSWORD 'weight_tracker';
-EOSQL"
+docker exec postgres bash -c 'psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+    CREATE DATABASE "$WEIGHT_TRACKER_DB_NAME";
+    CREATE USER "$WEIGHT_TRACKER_DB_USERNAME";
+    ALTER DATABASE "$WEIGHT_TRACKER_DB_NAME" OWNER TO "$WEIGHT_TRACKER_DB_USERNAME";
+    GRANT ALL PRIVILEGES ON DATABASE "$WEIGHT_TRACKER_DB_NAME" to "$WEIGHT_TRACKER_DB_USERNAME";
+    ALTER USER "$WEIGHT_TRACKER_DB_USERNAME" WITH PASSWORD "$WEIGHT_TRACKER_DB_PASSWORD";
+EOSQL'
 
 docker exec weighttracker-backend bash -c \
 "

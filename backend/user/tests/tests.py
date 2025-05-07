3bde6ed96d90800f17d918f1808bbb06ff436148
David <david@sulaco.co.uk>
2025-05-07 17:36:47 +0100

    remove old tests

--- backend/user/tests/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class TestsTest(TestCase):
-    def test_test(self):
-        """test that month ago is calculated correctly"""
-        self.assertEqual(True, False)
